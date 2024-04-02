f2e166a5533dab6a6ae5971ef0efe0a22904a5c3
kirandeep7889 <124796483+kirandeep7889@users.noreply.github.com>
2024-04-02 09:38:01 +0530

    added backend

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,28 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { IconButton, Menu, MenuItem } from '@material-ui/core';
-import { FiMenu } from 'react-icons/fi';
-import "./Header.css";
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUserDetails } from '../../Redux/userDetailsSlice';
+import "./Header.css";
 
 const Header = ({userType}) => {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const dispatch=useDispatch();
-  const user=useSelector(store=>store.userDetails.user)
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const dispatch = useDispatch();
+  const user = useSelector(store => store.userDetails.user);
 
   useEffect(() => {
     dispatch(fetchUserDetails());
   }, [dispatch]);
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
 
   return (
     <div className="Header">
@@ -38,38 +31,27 @@ const Header = ({userType}) => {
           <li><NavLink to="/signup" activeClassName="active">Signup</NavLink></li>
           <li><NavLink to="/login" activeClassName="active">Login</NavLink></li>
         </ul>
-        <IconButton
-          aria-controls="menu"
-          aria-haspopup="true"
-          onClick={handleClick}
-        >
-          <FiMenu />
-        </IconButton>
-        {/* Conditional rendering of the Menu */}
-        {user && (
-          <Menu
-            id="menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
+        <button className="menu-button" onClick={toggleMenu}>
+          User/Seller
+        </button>
+        {user && isMenuOpen && (
+          <div className="menu">
             { !user.isSeller ? (
-              [
-                <h2 key="customer">CUSTOMER</h2>,
-                <MenuItem key="cart" className="MenuItem" onClick={handleClose}><NavLink to="/cart" style={{ textDecoration: 'none' }}>Go to Cart</NavLink></MenuItem>,
-                <MenuItem key="orders" className="MenuItem" onClick={handleClose}><NavLink to="/orders" style={{ textDecoration: 'none' }}>Your Order Details</NavLink></MenuItem>,
-                <MenuItem key="wishlist" className="MenuItem" onClick={handleClose}><NavLink to="/wishlist" style={{ textDecoration: 'none' }}>Your Wishlist Items</NavLink></MenuItem>
-              ]
+              <div>
+                <h2>CUSTOMER</h2>
+                <NavLink to="/cart" className="MenuItem" onClick={toggleMenu}>Go to Cart</NavLink>
+                <NavLink to="/orders" className="MenuItem" onClick={toggleMenu}>Your Order Details</NavLink>
+                <NavLink to="/wishlist" className="MenuItem" onClick={toggleMenu}>Your Wishlist Items</NavLink>
+              </div>
             ) : (
-              [
-                <h2 key="seller">SELLER</h2>,
-                <MenuItem key="addProduct" className="MenuItem" onClick={handleClose}><NavLink to="/seller/addProduct" style={{ textDecoration: 'none' }}>Add Product</NavLink></MenuItem>,
-                <MenuItem key="showProducts" className="MenuItem" onClick={handleClose}><NavLink to="/seller/showProducts" style={{ textDecoration: 'none' }}>Display Products</NavLink></MenuItem>,
-                <MenuItem key="ordersPage" className="MenuItem" onClick={handleClose}><NavLink to="/orders-page" style={{ textDecoration: 'none' }}>Orders Page</NavLink></MenuItem>
-              ]
+              <div>
+                <h2>SELLER</h2>
+                <NavLink to="/seller/addProduct" className="MenuItem" onClick={toggleMenu}>Add Product</NavLink>
+                <NavLink to="/seller/showProducts" className="MenuItem" onClick={toggleMenu}>Display Products</NavLink>
+                <NavLink to="/orders-page" className="MenuItem" onClick={toggleMenu}>Orders Page</NavLink>
+              </div>
             )}
-          </Menu>
+          </div>
         )}
       </div>
     </div>
